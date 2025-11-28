4773bdfc6ad591ec545985755cb47c471d611429
prashanth1276 <prashanths272005@gmail.com>
2025-11-28 20:25:38 +0530

    Updated the path in extract_sample.py file

--- utils/extract_sample.py
+++ utils/extract_sample.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('Dataset/online_retail.csv')
+df = pd.read_csv('../Dataset/online_retail.csv')
 
 sample_df = df.head(15000)
 
-sample_df.to_csv('Dataset/retail_15000.csv', index=False)
+sample_df.to_csv('../Dataset/retail_15000.csv', index=False)
 
 print(f"Created sample with {len(sample_df)} records.")
 print(f"File Size: {df.memory_usage(deep=True).sum()/1024/1024:.2f} MB.")
